1326e31f3ce7b65dfedbf8b3730b6292d6810e4e
Rahul Rai <rahulraibit@gmail.com>
2021-11-20 17:13:57 +0530

    test

--- server/routes/masterRouter.js
+++ server/routes/masterRouter.js
@@ -5,7 +5,8 @@ let dashboardservice = require('../service/masterspec.js')
 router.get('/get', function (req, res) {
     console.log('reached')
     let l = dashboardservice.getData();
-    if(l || !l.length)
+    console.log(l);
+    if(!l || !l.length)
      {
          dashboardservice.createRecord();
      }
@@ -17,6 +18,12 @@ router.get('/get', function (req, res) {
 router.get('/search', function (req, res) {
     console.log("reache", req.query);
     let data = dashboardservice.getFilteredData(req.query.title);
+    res.send(data);
+});
+
+router.post('/create', function (req, res) {
+    console.log("reache", req.body);
+    let data = dashboardservice.createNewRecord(req.body);
     res.send(data);
 });
 
--- server/service/masterspec.js
+++ server/service/masterspec.js
@@ -6,13 +6,13 @@ let dashboardserviceClass = class BaseService {
 
     createRecord() {
        this.data = [ 
-            {'title': 'NET Development Project', 'desc': 'wruyer', status: 'active', liked: '20', imgSrc:"https://picsum.photos/id/237/200/300"},
-            {'title': 'Java Devlopment Project', 'desc': 'wruyer', status: 'active', liked: '20', imgSrc:"https://picsum.photos/id/238/200/300"},
-            {'title': 'Python Devlopment', 'desc': 'wruyer', status: 'active', liked: '20', imgSrc:"https://picsum.photos/id/239/200/300"},
-            {'title': 'HTML Development Project', 'desc': 'wruyer', status: 'active', liked: '20', imgSrc:"https://picsum.photos/id/240/200/300"},
-            {'title': 'Angular Development Project', 'desc': 'wruyer', status: 'active', liked: '20', imgSrc:"https://picsum.photos/id/227/200/300"},
-            {'title': 'React Development Project', 'desc': 'wruyer', status: 'active', liked: '20', imgSrc:"https://picsum.photos/id/217/200/300"},
-            {'title': 'Graphql Development Project', 'desc': 'wruyer', status: 'active', liked: '20', imgSrc:"https://picsum.photos/id/241/200/300"},
+            {'title': 'NET Project', 'desc': 'This project will be used for API creation', status: 'active', liked: '5', imgSrc:"https://picsum.photos/id/237/200/300"},
+            {'title': 'Java Project', 'desc': 'This project will be used for API creation', status: 'not started', liked: '20', imgSrc:"https://picsum.photos/id/238/200/300"},
+            {'title': 'Python ', 'desc': 'This project will be used for API creation', status: 'completed', liked: '10', imgSrc:"https://picsum.photos/id/239/200/300"},
+            {'title': 'HTML Project', 'desc': 'This project will be used for API static UI', status: 'active', liked: '1', imgSrc:"https://picsum.photos/id/240/200/300"},
+            {'title': 'Angular Project', 'desc': 'This project will be used for SPA', status: 'active', liked: '26', imgSrc:"https://picsum.photos/id/227/200/300"},
+            {'title': 'React Project', 'desc': 'This project will be used for SPA', status: 'active', liked: '3', imgSrc:"https://picsum.photos/id/217/200/300"},
+            {'title': 'Graphql Project', 'desc': 'This project will be used for API creation', status: 'active', liked: '4', imgSrc:"https://picsum.photos/id/241/200/300"},
         ];
         
     }
@@ -23,6 +23,17 @@ let dashboardserviceClass = class BaseService {
 
     getData() {
         return this.data
+    }
+
+    createNewRecord(item) {
+        this.data.push({
+            title: item.title,
+            desc: item.desc,
+            imgSrc : `https://picsum.photos/id/${Math.floor((Math.random() * 200) + 1)}/200/300`,
+            liked : 0,
+            status : item.status
+        })
+        console.log(this.data);
     }
 }
 let dashboardservice = new dashboardserviceClass()
