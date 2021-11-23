d950574795eeeac2de9074c82c9124907e8adfe7
rdev88 <ranudev89@gmail.com>
2021-11-23 21:02:59 +0000

    Added API for get/update and like

--- server/routes/masterRouter.js
+++ server/routes/masterRouter.js
@@ -3,16 +3,13 @@ var router = express.Router()
 let dashboardservice = require('../service/masterspec.js')
 
 router.get('/get', function (req, res) {
-    console.log('reached')
     let l = dashboardservice.getData();
     console.log(l);
-    if(!l || !l.length)
-     {
-         dashboardservice.createRecord();
-     }
-     l = dashboardservice.getData();
-     console.log('xxx', l)
-     res.send(l);
+    if (!l || !l.length) {
+        dashboardservice.createRecord();
+    }
+    l = dashboardservice.getData();
+    res.send(l);
 });
 
 router.get('/search', function (req, res) {
@@ -24,6 +21,21 @@ router.get('/search', function (req, res) {
 router.post('/create', function (req, res) {
     console.log("reache", req.body);
     let data = dashboardservice.createNewRecord(req.body);
+    res.send(data);
+});
+
+router.post('/product/:id', function (req, res) {
+    let data = dashboardservice.updateDataByID(req.params.id, req.body);
+    res.send(data);
+});
+
+router.get('/product/:id', function (req, res) {
+    let data = dashboardservice.getDataByID(req.params.id);
+    res.send(data);
+});
+
+router.post('/product/:id/like', function (req, res) {
+    let data = dashboardservice.incrementLike(req.params.id);
     res.send(data);
 });
 
--- server/service/masterspec.js
+++ server/service/masterspec.js
@@ -5,13 +5,13 @@ let dashboardserviceClass = class BaseService {
     };
 
     createRecord() {
-       this.data = [ 
-            {'id': 1, 'title': 'NET Project', 'desc': 'This project will be used for API creation', status: 'active', liked: '5', imgSrc:"https://picsum.photos/id/237/200/300"},
-            {'id': 2,'title': 'Java Project', 'desc': 'This project will be used for API creation', status: 'not started', liked: '20', imgSrc:"https://picsum.photos/id/238/200/300"},
-            {'id': 3,'title': 'Python ', 'desc': 'This project will be used for API creation', status: 'completed', liked: '10', imgSrc:"https://picsum.photos/id/239/200/300"},
-            {'id': 4,'title': 'HTML Project', 'desc': 'This project will be used for API static UI', status: 'active', liked: '1', imgSrc:"https://picsum.photos/id/240/200/300"}
+        this.data = [
+            { 'id': 1, 'title': 'NET Project', 'desc': 'This project will be used for API creation', status: 'active', liked: '5', imgSrc: "https://picsum.photos/id/237/200/300" },
+            { 'id': 2, 'title': 'Java Project', 'desc': 'This project will be used for API creation', status: 'not started', liked: '20', imgSrc: "https://picsum.photos/id/238/200/300" },
+            { 'id': 3, 'title': 'Python ', 'desc': 'This project will be used for API creation', status: 'completed', liked: '10', imgSrc: "https://picsum.photos/id/239/200/300" },
+            { 'id': 4, 'title': 'HTML Project', 'desc': 'This project will be used for API static UI', status: 'active', liked: '1', imgSrc: "https://picsum.photos/id/240/200/300" }
         ];
-        
+
     }
     getFilteredData(searchName) {
         console.log(searchName)
@@ -22,14 +22,39 @@ let dashboardserviceClass = class BaseService {
         return this.data
     }
 
+    getDataByID(id) {
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i][id] === id) {
+                return this.data[i][id];
+            }
+        }
+    }
+
+    updateDataByID(id, data) {
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i][id] === id) {
+                this.data[i] = { ...this.data[i], ...data };
+            }
+        }
+    }
+
+    incrementLike(id) {
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i][id] === id) {
+                let likeCount = 'liked' in this.data[i] ? this.data[i]['liked'] : 0;
+                this.data[i] = { ...this.data[i], like: likeCount + 1 };
+            }
+        }
+    }
+
     createNewRecord(item) {
         this.data.push({
-            id : this.data.length + 1,
+            id: this.data.length + 1,
             title: item.title,
             desc: item.desc,
-            imgSrc : `https://picsum.photos/id/${Math.floor((Math.random() * 200) + 1)}/200/300`,
-            liked : 0,
-            status : item.status
+            imgSrc: `https://picsum.photos/id/${Math.floor((Math.random() * 200) + 1)}/200/300`,
+            liked: 0,
+            status: item.status
         })
         console.log(this.data);
     }
