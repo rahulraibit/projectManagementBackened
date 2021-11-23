35f342e0ffbf3107fbceeea0a6611af0ca7e2dbe
Rahul Rai <rahulraibit@gmail.com>
2021-11-23 21:17:03 +0530

    code bug fix

--- server/service/masterspec.js
+++ server/service/masterspec.js
@@ -6,13 +6,10 @@ let dashboardserviceClass = class BaseService {
 
     createRecord() {
        this.data = [ 
-            {'title': 'NET Project', 'desc': 'This project will be used for API creation', status: 'active', liked: '5', imgSrc:"https://picsum.photos/id/237/200/300"},
-            {'title': 'Java Project', 'desc': 'This project will be used for API creation', status: 'not started', liked: '20', imgSrc:"https://picsum.photos/id/238/200/300"},
-            {'title': 'Python ', 'desc': 'This project will be used for API creation', status: 'completed', liked: '10', imgSrc:"https://picsum.photos/id/239/200/300"},
-            {'title': 'HTML Project', 'desc': 'This project will be used for API static UI', status: 'active', liked: '1', imgSrc:"https://picsum.photos/id/240/200/300"},
-            {'title': 'Angular Project', 'desc': 'This project will be used for SPA', status: 'active', liked: '26', imgSrc:"https://picsum.photos/id/227/200/300"},
-            {'title': 'React Project', 'desc': 'This project will be used for SPA', status: 'active', liked: '3', imgSrc:"https://picsum.photos/id/217/200/300"},
-            {'title': 'Graphql Project', 'desc': 'This project will be used for API creation', status: 'active', liked: '4', imgSrc:"https://picsum.photos/id/241/200/300"},
+            {'id': 1, 'title': 'NET Project', 'desc': 'This project will be used for API creation', status: 'active', liked: '5', imgSrc:"https://picsum.photos/id/237/200/300"},
+            {'id': 2,'title': 'Java Project', 'desc': 'This project will be used for API creation', status: 'not started', liked: '20', imgSrc:"https://picsum.photos/id/238/200/300"},
+            {'id': 3,'title': 'Python ', 'desc': 'This project will be used for API creation', status: 'completed', liked: '10', imgSrc:"https://picsum.photos/id/239/200/300"},
+            {'id': 4,'title': 'HTML Project', 'desc': 'This project will be used for API static UI', status: 'active', liked: '1', imgSrc:"https://picsum.photos/id/240/200/300"}
         ];
         
     }
@@ -27,6 +24,7 @@ let dashboardserviceClass = class BaseService {
 
     createNewRecord(item) {
         this.data.push({
+            id : this.data.length + 1,
             title: item.title,
             desc: item.desc,
             imgSrc : `https://picsum.photos/id/${Math.floor((Math.random() * 200) + 1)}/200/300`,
